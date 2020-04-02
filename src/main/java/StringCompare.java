0dc021dd1c00395b297b369e755a79561dfa37d9
kesava07 <kesava07.ip@gmail.com>
2020-04-02 16:37:53 -0700

    checkstyle improvement

--- src/main/java/StringCompare.java
+++ src/main/java/StringCompare.java
@@ -1,7 +1,14 @@
 public class StringCompare {
-    public static void main(String[] args) {
-        String[] str={"ved","hej","hj","ko"};
-        if (str[0]==str[3]){
+    public static void main(String[] args)
+    {
+        String[] str={"ved","hej","hj","ko"} ;
+
+
+
+        if (str[0].equals(str[3])){
+
+
+
             System.out.println(true);
         }
     }
